d129f41f4a04a9010e0776dc5275f9eccb9023b9
PupilDark <sdong@uwm.edu>
2018-04-21 22:42:50 -0500

    Add files via upload

--- scampi.github.io_files/yasqe.bundled.js
+++ scampi.github.io_files/yasqe.bundled.js
@@ -27891,9 +27891,45 @@ module.exports = function(YASQE, yasqe) {
   };
 
   var getLocalDefinition = function(subject, predicate){
+	if(subject){
+		var list = localDefinitions;
+		list[subject] = list[subject] || {};
+		if(predicate){
+			list = list[subject];
+			list[predicate] = list[predicate] || [];
   };
 
   var removeLocalDefinition = function(subject, predicate, object){
+	if(subject){
+		var list = localDefinitions;
+		list[subject] = list[subject] || {};
+		if(predicate){
+			list = list[subject];
+			list[predicate] = list[predicate] || [];
+			if(object){
+				list = list[predicate];
+				if(typeof object == 'table'){
+					var data;
+					for(var k in list){
+						data = list[k];
+						if(typeof data == 'table'){
+							if(data[0] == object[0] && data[1] == object[1]){
+								return;
+							}
+						}
+					}
+				}
+				else{
+					for(var k in list){
+						if(list[k] == object){
+							return;
+						}
+					}
+				}
+				list.remove(object);
+			}
+		}
+	}
   };
 
 	var partialMatchFilter = function(list, token){
