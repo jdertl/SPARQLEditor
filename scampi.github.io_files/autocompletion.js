732a854aa38dd344e995bd65e9f314bb6a044c19
jdertl <jdertl@uwm.edu>
2018-04-29 12:35:43 -0500

    Split "local_definitions" completer for implementation

--- scampi.github.io_files/autocompletion.js
+++ scampi.github.io_files/autocompletion.js
@@ -106,45 +106,6 @@ YASQE.registerAutocompleter("test", function(yasqe){
 	};
 })
 
-YASQE.registerAutocompleter("local_definitions", function(yasqe){
-	var classDesignators = {"a": true, "rdf:type": true, "https://www.w3.org/1999/02/22-rdf-syntax-ns#type": true};
-	return{
-		isValidCompletionPosition: function(){
-			return yasqe.getTriples(true, true) != null;
-		},
-		get: {
-			getSubject: function(context, subjectClass, subject, predicate, object, token){
-				return yasqe.autocompleters.partialMatchFilter(yasqe.autocompleters.getLocalDefinition(), subject);
-			},
-			getPredicate: function(context, subjectClass, subject, predicate, object, token){
-				if(subjectClass){
-					var suggestLevel = yasqe.autocompleters.getLocalDefinition(subjectClass);
-					if(suggestLevel){
-						return yasqe.autocompleters.partialMatchFilter(suggestLevel, predicate);
-					}
-				}
-				return [];
-			},
-			getObject: function(context, subjectClass, subject, predicate, object, token){
-				if(classDesignators[predicate]){
-					return yasqe.autocompleters.partialMatchFilter(yasqe.autocompleters.getLocalDefinition(), object);
-				}
-				else if(subjectClass && predicate){
-					var suggestLevel = yasqe.autocompleters.getLocalDefinition(subjectClass, predicate);
-					if(suggestLevel){
-						return yasqe.autocompleters.partialMatchFilter(suggestLevel, object);
-					}
-				}
-				return [];
-			}
-		},
-		async: false,
-		bulk: false,
-		autoShow: true,
-		getUsesCompleteToken: true
-	};
-});
-
 YASQE.defaults.autocompleters = [
 	// "prefixes",
 	"variables",
